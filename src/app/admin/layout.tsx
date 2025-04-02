0a34f4d9abfe38bba70d079d31c77745845eca71
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-02 23:51:31 +0530

    fix(admin): layout name fix

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import React from "react";
 
-function AdminPageLayout({ children }: { children: React.ReactNode }) {
+function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col items-center w-full">
       <Header />
@@ -12,4 +12,4 @@ function AdminPageLayout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default AdminPageLayout;
+export default AdminLayout;
