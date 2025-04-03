7da2c4d7ae4659a69db65bc997b1fa2225cf2484
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-03 09:02:26 +0530

    fix(api): added dynamic checking for hc url

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,6 +1,10 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-const client = hc<AppType>("http://localhost:3000");
+const client = hc<AppType>(
+  process.env.NODE_ENV === "production"
+    ? "https://pf-marathon-16.fly.dev"
+    : "http://localhost:3000"
+);
 
 export const api = client.api;
