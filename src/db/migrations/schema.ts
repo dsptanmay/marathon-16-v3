52e9c066c9cfbe2a6a55a795d052aaa980561bf8
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-05 12:57:36 +0530

    fix(project): fixes and merging of actions

--- src/db/migrations/schema.ts
+++ src/db/migrations/schema.ts
@@ -1,19 +1,32 @@
-import { pgTable, unique, uuid, text, timestamp, boolean, pgEnum } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import {
+  pgTable,
+  unique,
+  uuid,
+  text,
+  timestamp,
+  boolean,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 
-export const category = pgEnum("category", ['girls', 'boys', 'walkathon_m', 'walkathon_f'])
-
-
-export const master = pgTable("master", {
-	id: uuid().defaultRandom().primaryKey().notNull(),
-	uniqueCode: text("unique_code").notNull(),
-	name: text().notNull(),
-	email: text().notNull(),
-	phoneNo: text("phone_no").notNull(),
-	usn: text(),
-	category: category().notNull(),
-	crossTime: timestamp({ withTimezone: true, mode: 'string' }),
-	isSitian: boolean().default(false),
-}, (table) => [
-	unique("master_unique_code_unique").on(table.uniqueCode),
+export const category = pgEnum("category", [
+  "girls",
+  "boys",
+  "walkathon_m",
+  "walkathon_f",
 ]);
+
+export const master = pgTable(
+  "master",
+  {
+    id: uuid().defaultRandom().primaryKey().notNull(),
+    uniqueCode: text("unique_code").notNull(),
+    name: text().notNull(),
+    email: text().notNull(),
+    phoneNo: text("phone_no").notNull(),
+    usn: text(),
+    category: category().notNull(),
+    crossTime: timestamp({ withTimezone: true, mode: "string" }),
+    isSitian: boolean().default(false),
+  },
+  (table) => [unique("master_unique_code_unique").on(table.uniqueCode)]
+);
