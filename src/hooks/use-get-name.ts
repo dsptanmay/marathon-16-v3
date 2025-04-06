5985aa54ceb022c854625cbd6157010736c3a0ca
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-06 13:34:38 +0530

    fix(hooks): using hono RPC now instead of axios

--- src/hooks/use-get-name.ts
+++ src/hooks/use-get-name.ts
@@ -1,22 +1,21 @@
 import { api } from "@/lib/hono";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { InferResponseType } from "hono";
 
 type $get = typeof api.utils.crossed.$get;
-type ResponseType = InferResponseType<$get, 200>;
+type ResponseType = InferResponseType<$get, 200>["data"];
 
 export const useGetName = (submitted_code: string | null) => {
   const query = useQuery<ResponseType, Error>({
     queryKey: ["isCrossed", { submitted_code }],
     enabled: !!submitted_code,
     queryFn: async () => {
-      const response = await axios.get<ResponseType>(
-        `/api/utils/crossed?unique_code=${submitted_code}`
-      );
-      if (response.status !== 200)
+      const response = await api.utils.crossed.$get({
+        query: { unique_code: submitted_code! },
+      });
+      if (!response.ok)
         throw new Error("Failed to check if participant has crossed!");
-      return response.data;
+      return (await response.json()).data;
     },
   });
   return query;
