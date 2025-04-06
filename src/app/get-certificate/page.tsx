8483991ce7449d902d5afd4aefd1e4c42feb9af4
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-06 13:58:18 +0530

    feat(certificate): added certificate generation to the page; still needs tinkering

--- src/app/get-certificate/page.tsx
+++ src/app/get-certificate/page.tsx
@@ -8,19 +8,73 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+
 import { useGetName } from "@/hooks/use-get-name";
-import React, { useState } from "react";
+
+import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
+
+import React, { useEffect, useState } from "react";
 
 function GetCertificatePage() {
   const [code, setCode] = useState("");
-  const [submittedCode, setSubmittedCode] = useState<string | null>(null);
+  const [certBytes, setCertBytes] = useState<ArrayBuffer>();
 
-  const { data, isLoading } = useGetName(submittedCode);
+  const { isLoading, status, error, refetch } = useGetName(code);
+
+  useEffect(() => {
+    const fetchCertificate = async () => {
+      const certBuffer = await fetch("https://i.imgur.com/T7AMnkD.png").then(
+        (img) => img.arrayBuffer()
+      );
+      setCertBytes(certBuffer);
+    };
+
+    fetchCertificate();
+  }, []);
+
+  const generatePDF = async (name: string) => {
+    const doc = await PDFDocument.create();
+    const page = doc.addPage([3508, 2481]);
+    const font = await doc.embedFont(StandardFonts.TimesRomanBoldItalic);
+    const fontSize = 100;
+    page.setFont(font);
+
+    const textWidth = font.widthOfTextAtSize(name, fontSize);
+    const { width } = page.getSize();
+
+    const certImage = await doc.embedPng(certBytes!);
+
+    page.drawImage(certImage, { x: 0, y: 0, height: 2481, width: 3508 });
+
+    page.drawText(name, {
+      x: (width - textWidth) / 2,
+      y: 1275,
+      size: fontSize,
+      color: rgb(0, 0, 0),
+      font,
+    });
+
+    const pdfBytes = await doc.save();
+    return pdfBytes;
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setSubmittedCode(code);
-    console.log(data);
+    const { data } = await refetch();
+    if (status === "error") {
+      console.error(error);
+      return;
+    }
+    if (status === "success") {
+      const pdf = await generatePDF(data!);
+      const blob = new Blob([pdf], { type: "application/pdf" });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `Certificate_Of_Participation-${data}.pdf`;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    }
   };
   return (
     <div className="p-6 w-full lg:max-w-lg">
