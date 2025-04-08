46d2cbddb53fb889852ad46e04e8c1155defa23a
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-08 22:33:30 +0530

    fixed the issue with build

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,8 +1,6 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-const client = hc<AppType>(
-  "https://pf-marathon-16.vercel.app/"
-);
+const client = hc<AppType>("https://pf-marathon-16.vercel.app/");
 
 export const api = client.api;
