acac66c5641ecd7c38413b32138b7c73d881a4e7
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-09 21:19:56 +0530

    one last fix, I think

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,6 +1,5 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
-import { config } from "dotenv";
 
 const client = hc<AppType>("https://pf-marathon-16.vercel.app/");
 
