3a34d193ebdfc9bfab57ab343162104365c42b62
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-10 19:37:12 +0530

    fix(css): updated components to work with tailwind v4

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -1,13 +1,13 @@
 export default function Marquee({ items }: { items: string[] }) {
   return (
-    <div className="relative flex w-full overflow-x-hidden border-b-2 border-t-2 border-border bg-bw text-text font-base">
+    <div className="relative flex w-full overflow-x-hidden border-b-2 border-t-2 border-border bg-secondary-background text-foreground font-base">
       <div className="animate-marquee whitespace-nowrap py-12">
         {items.map((item) => {
           return (
             <span key={item} className="mx-4 text-4xl">
               {item}
             </span>
-          )
+          );
         })}
       </div>
 
@@ -17,11 +17,11 @@ export default function Marquee({ items }: { items: string[] }) {
             <span key={item} className="mx-4 text-4xl">
               {item}
             </span>
-          )
+          );
         })}
       </div>
 
       {/* must have both of these in order to work */}
     </div>
-  )
+  );
 }
