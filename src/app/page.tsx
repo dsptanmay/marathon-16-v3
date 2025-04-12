aea6bcdcb8961690483ab67123bf70280125a5b1
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-04-12 11:23:59 +0530

    fix(lint): fixing linting issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import fakeLogo from "../../public/fake-logo.svg";
 import {
   Card,
   CardContent,
