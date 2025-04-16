0460fac0c4bdc7e6392a5d2fce73b3750dbf0426
Tanmay Deshpande <39451995+dsptanmay@users.noreply.github.com>
2025-04-16 12:51:06 +0530

    feat(components): added all participants button component

---
+++ src/components/admin/all/all-participants.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import useGetAllParticipants from "@/hooks/all/use-get-all";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import React from "react";
+
+function GetAllParticipantsButton() {
+  const { error, refetch, isFetching } = useGetAllParticipants();
+
+  const generatePDF = async (
+    data: {
+      unique_code: string;
+      name: string;
+      email: string;
+      phone_no: string;
+      time_crossed: string | null;
+    }[]
+  ) => {
+    const bodyValues = data.map(
+      ({ unique_code, name, email, phone_no, time_crossed }) => [
+        unique_code,
+        name,
+        email,
+        phone_no,
+        time_crossed ? new Date(time_crossed).toLocaleString() : null,
+      ]
+    );
+    const doc = new jsPDF({ orientation: "portrait" });
+    const pageWidth = doc.internal.pageSize.getWidth();
+
+    doc.setFontSize(16);
+    doc.text("All Marathon Participants", pageWidth / 2, 20, {
+      align: "center",
+    });
+    autoTable(doc, {
+      head: [["Unique Code", "Name", "Email", "Phone Number", "Time Crossed"]],
+      body: bodyValues,
+      theme: "striped",
+      startY: 30,
+    });
+    doc.save("All Participants - Marathon.pdf");
+  };
+
+  const handleSubmit = async () => {
+    await refetch()
+      .then((result) => {
+        if (result.status === "success" && result.data) {
+          return generatePDF(result.data);
+        }
+        if (result.status === "error" && error) {
+          console.error(error);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+  return (
+    <div>
+      <Button
+        className="text-base py-6 w-full"
+        variant="noShadow"
+        onClick={handleSubmit}
+      >
+        {isFetching ? "Fetching Data..." : "All Participants"}
+      </Button>
+    </div>
+  );
+}
+
+export default GetAllParticipantsButton;
